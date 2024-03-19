74550fcea1f32679dc838b3bff6718d4c74f088a
Your Namecd <you@example.com>
2024-03-19 14:05:17 -0400

    stops data

---
+++ cs178-output/data.js
@@ -0,0 +1,16 @@
+import React from "react";
+export const animals = [
+    {label: "SEC", value: "SEC"},
+    {label: "Harvard Business School", value: "Harvard Business School"},
+    {label: "Harvard Kennedy School", value: "Harvard Kennedy School"},
+    {label: "Harvard Law School", value: "Harvard Law School"},
+    {label: "Quad", value: "Quad"},
+    {label: "Eliot House", value: "Eliot House"},
+    {label: "Memorial Hall", value: "Memorial Hall"},
+    {label: "Maxwell Dorkins", value: "Maxwell Dorkins"},
+    {label: "Widener Gate", value: "Widener Gate"},
+    {label: "Lamont Library", value: "Lamont Library"},
+    {label: "Eliot House Ave", value: "Eliot House"},
+    {label: "Quincy House", value: "Quincy House"},
+    {label: "Mather House", value: "Mather House"}
+]

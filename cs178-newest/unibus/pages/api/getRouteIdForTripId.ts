33656c331a4054911538a739f2e22bb26ea0ff9b
Emmet Halm <emmet.halm@gmail.com>
2024-03-21 23:39:32 -0700

    debugging route

--- cs178-newest/unibus/pages/api/getRouteIdForTripId.ts
+++ cs178-newest/unibus/pages/api/getRouteIdForTripId.ts
@@ -1,23 +1,20 @@
-// Next.js API route to get stop_id from stop_name
+// Next.js API route to get route_id for trip_id
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const db = await open({ filename: './scripts/static_data.sqlite3', driver: sqlite3.Database });
-  // see if I'm accessing the query incorrectly
-  const stopName = req.query.stop_name;
-  // prints successfully
-  console.log("stopName", stopName);
+  const routeId = req.query.route_id;
+  console.log("stopName", routeId);
 
   try {
-    const stop = await db.get('SELECT stop_id FROM stops WHERE stop_name = ?', [stopName]);
-    res.status(200).json({ stopId: stop?.stop_id });
+    const trip = await db.get('SELECT route_id FROM trips WHERE trip_id = ?', [routeId]);
+    res.status(200).json({ tripId: trip?.trip_id });
     // Not printing
-    console.log("StopId: ", stop);
+    console.log("tripId: ", trip);
 
   } catch (error) {
-    // this error is printing
     res.status(500).json({ error: 'Error querying the database' });
   } finally {
     await db.close();
